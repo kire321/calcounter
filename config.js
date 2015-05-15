8cbcfe5c5cf2fbb8cd4b5ce07b6714c58b44c395
Erik Wyatt <wyatte@stolaf.edu>
2015-05-15 12:51:35 +0200

    improve detection of production

--- config.js
+++ config.js
@@ -1,22 +1,21 @@
 var fs = require('fs');
 
-exports.port = process.env.PORT || 8888 
-
-var ebconfig = null;
-try {
-	ebconfig = fs.readFileSync('local.config')
-} catch (error) {}
-if (ebconfig) {
+if (process.env.PORT) {
+	//production
+	exports.port = process.env.PORT
+	for (key in process.env) {
+		exports[key] = process.env[key];
+	}
+	exports.host = "calcounter-dev.elasticbeanstalk.com";
+} else {
+	//local
+	exports.port = 8888
+	var ebconfig = fs.readFileSync('local.config');
 	var configJSON = JSON.parse(ebconfig);
 	configJSON.option_settings.forEach(function (option) {
 		exports[option.option_name] = option.value;
 	});
 	exports.host = "localhost:" + exports.port;
-} else {
-	for (key in process.env) {
-		exports[key] = process.env[key];
-	}
-	exports.host = "calcounter-dev.elasticbeanstalk.com";
 }
 
 
